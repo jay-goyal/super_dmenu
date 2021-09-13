66bb53479433cbd51ccf38532f9ebacf03559c1e
Jay Goyal <goyal.jay2003@gmail.com>
2021-09-13 12:35:42 +0530

    First Commit

---
+++ config.def.h
@@ -0,0 +1,33 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Roboto:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#D0D0D0", "#292D3E" },
+	[SchemeSel] = { "#D0D0D0", "#82AADD" },
+	[SchemeSelHighlight] = { "#FFCB6B", "#82AADD" },
+	[SchemeNormHighlight] = { "#FFCB6B", "#292D3E" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 27;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
