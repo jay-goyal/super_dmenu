f0f00aff4cb48a0933b6b4f49b154b760c0f5247
Jay Goyal <goyal.jay2003@gmail.com>
2021-10-09 13:47:56 +0530

    Corrected the editor script

--- scripts/editor.sh
+++ scripts/editor.sh
@@ -1,10 +1,19 @@
 declare -a editors=(
-"CLion:-:clion"
-"Emacs:-:emacsclient -c"
-"NeoVim:-:nvim"
+"CLion"
+"Emacs"
+"Neovim"
 )
 
-choice=$(printf '%s\n' "${editors[@]}" | awk -F ':' '{print $1}' | dmenu -h 27 -l 20 -bw 1 -p "Edit Using: ")
+choice=$(printf '%s\n' "${editors[@]}" | dmenu -h 27 -l 20 -bw 1 -p "Edit Using: ")
 
-editor=$(printf '%s\n' "${choice}" | awk -F ':' '{print $NF}')
-$editor
+case $choice in
+    CLion)
+        clion
+        ;;
+    Emacs)
+        emacs
+        ;;
+    Neovim)
+        alacritty -e nvim
+        ;;
+esac
